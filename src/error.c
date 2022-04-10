721be1cfd272a06294b74dc128b3e671c1177c94
saccharineboi <saccharineboi@gmail.com>
2022-04-10 16:39:54 +0400

    Add date to error reporting

--- src/error.c
+++ src/error.c
@@ -2,6 +2,8 @@
 
 #include "error.h"
 
+#include <time.h>
+
 ////////////////////////////////////////
 static txMessageCallback messageCallback = NULL;
 
@@ -18,9 +20,18 @@ void txOutputMessage(enum TXmessageType type, const char* message, ...)
         va_list arg;
         va_start(arg, message);
 
+        time_t now = time(NULL);
+        struct tm* timeInfo = localtime(&now);
+
 #define FORMATTED_MESSAGE_MAX_SIZE 250
         char formattedMessage[FORMATTED_MESSAGE_MAX_SIZE];
-        vsnprintf(formattedMessage, FORMATTED_MESSAGE_MAX_SIZE, message, arg);
+        int c = snprintf(formattedMessage, FORMATTED_MESSAGE_MAX_SIZE, "[%d::%d::%d %d::%d::%d] ", timeInfo->tm_mon,
+                                                                                                   timeInfo->tm_mday,
+                                                                                                   timeInfo->tm_year + 1900,
+                                                                                                   timeInfo->tm_hour,
+                                                                                                   timeInfo->tm_min,
+                                                                                                   timeInfo->tm_sec);
+        vsnprintf(formattedMessage + c, FORMATTED_MESSAGE_MAX_SIZE, message, arg);
         va_end(arg);
 
         messageCallback(type, formattedMessage);
