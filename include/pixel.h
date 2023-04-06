85efc8a9093ed810b11bb1e546f80603434c5519
saccharineboi <saccharineboi@gmail.com>
2023-04-06 16:38:42 +0300

    Remove redundant comments from pixel.h

--- include/pixel.h
+++ include/pixel.h
@@ -11,11 +11,6 @@ extern "C" {
 #include "vec.h"
 
 ////////////////////////////////////////
-/// Instead of maintaining multiple buffers,
-/// we have a pixel object which stores all the
-/// information we need to render it properly
-/// onto the framebuffer
-////////////////////////////////////////
 struct TXpixel {
     TXvec4 color;
     float depth;
